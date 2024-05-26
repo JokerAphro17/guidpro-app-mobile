2e877a2b39f745e54562225dec448856610cdbae
Souley <souleymane.ilboudo@nafann.com>
2024-05-26 17:01:29 +0000

    article

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Board(),
+        home: Home(),
         routes: {
           '/login': (context) =>  LoginScreen(),
           '/signup': (context) =>  SignUpScreen(),
--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guidpro_mobile/constants/Theme.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -23,14 +24,75 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome to the home page'),
-          ],
+      body: ArticleList(),
+    );
+  }
+}
+
+class ArticleList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context, index) {
+        return ArticleItem();
+      },
+    );
+  }
+}
+
+class ArticleItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          // width: 90%,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(20))
+          ),
+
+
+
         ),
-      ),
+        // overlay
+
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            color: Colors.black.withOpacity(0.5),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Elevage de bovins',
+                  style: TextStyle(
+                    color: MaterialColors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Article Description',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
